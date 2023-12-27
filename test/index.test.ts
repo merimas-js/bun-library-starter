2a24016f00777361b40486d4f4affe9a9b74cbf3
Upsided <73066977+Upsidedly@users.noreply.github.com>
2023-12-27 00:01:10 -0500

    chore: fix test foolishness

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,8 @@
-import { describe, it, expect } from 'bun:test'
-import { one, two } from '../src'
+// import { test, expect } from 'bun:test'
+// import { ... } from '../src'
 
-describe('should', () => {
-  it('export 1', () => {
-    expect(one).toBe(1)
-  })
-
-  it('export 2', () => {
-    expect(two).toBe(2)
-  })
+/*
+test("Should a = b", () => {
+  expect(a).toBe(b)
 })
+*/
